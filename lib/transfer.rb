7307b4b9d84a63f7948ab79ec1fea4a65a67c647
Michael Dobak <mdobak44@icloud.com>
2021-03-01 23:46:35 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,8 +13,7 @@ class Transfer
   
   def execute_transaction
     if @sender.valid? 
-    @sender.balance -= @amount & @receiver.balance += @amount
-    @sender.balance
+    
     else 
     "Transaction rejected. Please check your account balance."
    end 
