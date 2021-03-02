faec5229472de7d17731cb377b6d3de88b33850e
Michael Dobak <mdobak44@icloud.com>
2021-03-02 00:12:04 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,12 +13,14 @@ class Transfer
   end 
   
   def execute_transaction
-    if valid? && @status == "pending"
+    if valid? && @status == "pending" 
       sender.balance -= amount
       receiver.balance += amount
       @status = "complete"
     else 
+    @status = "rejected"
     "Transaction rejected. Please check your account balance."
+
    end 
   end 
   
