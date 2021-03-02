daa6f440c32d5619f62b5f97c6165744648425d7
Michael Dobak <mdobak44@icloud.com>
2021-03-02 00:12:06 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,8 +18,8 @@ class Transfer
       receiver.balance += amount
       @status = "complete"
     else 
-    @status = "rejected"
-    "Transaction rejected. Please check your account balance."
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
 
    end 
   end 
